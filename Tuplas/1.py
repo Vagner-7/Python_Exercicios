e02bfc2c3fc6d63f2aef3b45bb18f3555576a6a6
Vagner-7 <130634047+Vagner-7@users.noreply.github.com>
2023-09-25 00:18:15 -0300

    Update 1.py

--- Tuplas/1.py
+++ Tuplas/1.py
@@ -3,15 +3,6 @@ for comida in lanche:
     print(f"Eu vou comer {comida}")
 print(f"Ao mossei {len(lanche)} lanches!")
 
-
-
-
-
-
-
-
-
-
 for pos, comida in enumerate(lanche):
     print(f"Eu vou al mossar {comida}, que está na possição {pos}")
 print("Al mossei, de buxo cheio")
